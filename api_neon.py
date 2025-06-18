8f16a9a8b64f7a9f32183dbbfc14359fe255f3fa
kauanhen02 <139480381+kauanhen02@users.noreply.github.com>
2025-06-18 12:28:07 -0300

    Update api_neon.py

--- api_neon.py
+++ api_neon.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import psycopg2
 
 app = FastAPI(
     title="Quitzau API",
-    description="API de produtos conectada ao banco Neon, retorna todos os produtos",
+    description="API de produtos com paginação conectada ao banco Neon",
     version="1.0.0",
     docs_url="/docs",
     openapi_url="/openapi.json"
@@ -20,24 +20,24 @@ def get_conn():
     )
 
 @app.get("/produtos")
-def listar_todos_produtos():
+def listar_produtos(page: int = Query(1, ge=1), page_size: int = Query(20, ge=1, le=100)):
+    offset = (page - 1) * page_size
+
     conn = get_conn()
     cur = conn.cursor()
 
     cur.execute("""
         SELECT pro_in_codigo, pro_st_descricao, re_custo
         FROM produtos
-    """)
+        ORDER BY pro_in_codigo
+        LIMIT %s OFFSET %s
+    """, (page_size, offset))
 
     resultados = cur.fetchall()
     cur.close()
     conn.close()
 
     return [
-        {
-            "codigo": row[0],
-            "descricao": row[1],
-            "custo": float(row[2])
-        }
-        for row in resultados
+        {"codigo": r[0], "descricao": r[1], "custo": float(r[2])}
+        for r in resultados
     ]
